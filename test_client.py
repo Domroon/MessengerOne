15da5e0a1fc45f5742d9db765b626d1d63509f02
Dominik Haeusser <haeusser-dominik@web.de>
2021-05-04 19:54:31 +0200

    add client name in message

--- test_client.py
+++ test_client.py
@@ -6,13 +6,13 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = "192.168.178.25"
 ADDR = (SERVER, PORT)
-
+CLIENT_NAME = "Domroon"
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
 
-    msg = input("Message: ")
+    msg = f"{CLIENT_NAME}: {input('Message: ')}"
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
