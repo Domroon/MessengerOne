a2f019e26e46429549e57cf803f0dbe60769cce3
Dominik Haeusser <haeusser-dominik@web.de>
2021-05-04 09:14:35 +0200

    add send message

--- client.py
+++ client.py
@@ -8,9 +8,19 @@ SERVER = "192.168.43.168"
 ADDR = (SERVER, PORT)
 
 
+def send(msg, client):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
+
+    send("Hello World!", client)
 
 if __name__ == '__main__':
     main()
