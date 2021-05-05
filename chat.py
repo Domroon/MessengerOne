ef89969921f4c92f9c46def6782c22e99784e988
Dominik Haeusser <haeusser-dominik@web.de>
2021-05-05 09:21:01 +0200

    put all functions in a class

--- chat.py
+++ chat.py
@@ -10,67 +10,79 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 CLIENT_NAME = "HOST"
 
+class Chat:
+    def __init__(self):
+        self.message = ""
+        self.target_ip = 0
 
-def receive_message(conn):
-    msg_length = conn.recv(HEADER).decode(FORMAT)
-    if msg_length:
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        return msg
+    def receive_message(self, conn):
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            return msg
 
+    def send_message(self, conn, msg):
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        conn.send(send_length)
+        conn.send(message)
 
-def send_message(conn, msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    conn.send(send_length)
-    conn.send(message)
+    def receive(self):
+        receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        receive_socket.bind(ADDR)
 
+        print("[STARTING] Receiver is starting...")
+        
+        receive_socket.listen()
+        print(f"[LISTENING] Receiver is listening on {SERVER}:{PORT}")
 
-def receive():
-    receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    receive_socket.bind(ADDR)
+        conn, addr = receive_socket.accept()
+        self.target_ip = addr
+        print(f"[NEW CONNECTION] {addr} connected.")
+        while True:
+            self.message = self.receive_message(conn)
+            print(self.message)
+            if self.message == DISCONNECT_MESSAGE:
+                conn.close()
+                break
 
-    print("[STARTING] Receiver is starting...")
-    
-    receive_socket.listen()
-    print(f"[LISTENING] Receiver is listening on {SERVER}")
+    def transreceive(self, ip_adress):
+        transreceive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transreceive_socket.connect((ip_adress, PORT))
 
-    conn, addr = receive_socket.accept()
-    print(f"[NEW CONNECTION] {addr} connected.")
-    while True:
-        message = receive_message(conn)
-        print(message)
-        if message == DISCONNECT_MESSAGE:
-            conn.close()
-            break
+        print(f"[CONNECTED] Transceiver connected to {ip_adress}")
+        name = input("Enter your Nickname: ")
+        while True:
+            user_msg = input('Message: ')
+            msg = f"{name}: {user_msg}"
+            self.send_message(transreceive_socket, msg)
+            if user_msg == "exit":
+                self.send_message(transreceive_socket, DISCONNECT_MESSAGE)
+                break
 
+    def wait_client(self):
+        self.receive()
 
-def transreceive(ip_adress):
-    transreceive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transreceive_socket.connect((ip_adress, PORT))
-
-    print(f"[CONNECTED] Transceiver connected to {ip_adress}")
-    name = input("Enter your Nickname: ")
-    while True:
-        user_msg = input('Message: ')
-        msg = f"{name}: {user_msg}"
-        send_message(transreceive_socket, msg)
-        if user_msg == "exit":
-            send_message(transreceive_socket, DISCONNECT_MESSAGE)
-            break
+    def search_client(self):
+        pass
 
 
 def main():
+    chat = Chat()
+    #chat.wait_client()
+    
     print("1 - Receiver\n2 - Transreceiver")
     choice = input("Input: ")
     if choice == '1':
-        receive()
+        chat.receive()
     elif choice == '2':
-        transreceive(input("IP Adress: "))
+        chat.transreceive(input("IP Adress: "))
     else:
         print("Wrong Input")
+    
 
 
 if __name__ == '__main__':
