3a6d44c97e220479a334323686c1ea9a77d03162
Dominik Haeusser <haeusser-dominik@web.de>
2021-05-09 09:46:36 +0200

    delete 'receive_message'-method in Client

--- messengerTwo.py
+++ messengerTwo.py
@@ -41,6 +41,60 @@ class Server:
             print(f"[RECEIVE] from {address}: {client_message}")
 
 
+class Client:
+    def __init__(self):
+        self.communication_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+         
+    def send_message(self, message_decoded):
+        message = (message_decoded + "\n").encode()
+        self.communication_socket.sendall(message)
+
+    def start(self):
+        print("[STARTING] Client is starting ... ")
+
+        ip_address = input("IP Address: ")
+        port = int(input("Port: "))
+
+        while True:
+            try:
+                self.communication_socket.connect((ip_address, port))
+                break
+            except TimeoutError:
+                print(f"\nCant't connect to {ip_address}:{port}")
+                print("The server is probably not online.")
+                self.user_query()
+            except ConnectionRefusedError:
+                print(f"\nI can connect to {ip_address},")
+                print(f"but no server is listening on port {port}")
+                self.user_query()
+
+        # Receiving Welcome Message
+        print(receive_message(self.communication_socket, (ip_address, port)))
+
+        print(f"[CONNECTED] Connected with server ('{ip_address}':{port})")
+
+        # User can send messages
+        while True:
+            user_message = input()
+            self.send_message(user_message)
+            if user_message == 'q':
+                self.send_message("!DISCONNECT")
+                print(receive_message(self.communication_socket, (ip_address, port)))
+                break
+        
+        self.communication_socket.close()
+
+    def user_query(self):
+        print("Do you want to try it again?")
+        print("y - Yes")
+        print("n - No")
+        user_input = input()
+        if user_input == 'y':
+            return
+        elif user_input == 'n':
+            exit()
+
+
 def receive_message(communication_socket, address):
         message = ""
         receive_message = True
@@ -68,74 +122,6 @@ def send_message(message_decoded, communication_socket):
         message = (message_decoded + "\n").encode()
         communication_socket.sendall(message)
 
-
-class Client:
-    def __init__(self):
-        self.communication_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-         
-    def receive_message(self):
-        message = ""
-        receive_message = True
-        while receive_message:
-            #receive 2 bytes
-            bytes = self.communication_socket.recv(2)
-            # decode the bytes to utf-8 and search for newline-sign
-            bytes_decoded = bytes.decode("utf-8")
-            for sign in bytes_decoded:
-                if sign == "\n":
-                    bytes_decoded = bytes_decoded[:-1]
-                    receive_message = False
-            message += bytes_decoded
-        return message
-
-    def send_message(self, message_decoded):
-        message = (message_decoded + "\n").encode()
-        self.communication_socket.sendall(message)
-
-    def start(self):
-        print("[STARTING] Client is starting ... ")
-
-        ip_address = input("IP Address: ")
-        port = int(input("Port: "))
-
-        while True:
-            try:
-                self.communication_socket.connect((ip_address, port))
-                break
-            except TimeoutError:
-                print(f"\nCant't connect to {ip_address}:{port}")
-                print("The server is probably not online.")
-                self.user_query()
-            except ConnectionRefusedError:
-                print(f"\nI can connect to {ip_address},")
-                print(f"but no server is listening on port {port}")
-                self.user_query()
-
-        # Receiving Welcome Message
-        print(self.receive_message())
-
-        print(f"[CONNECTED] Connected with server ('{ip_address}':{port})")
-
-        # User can send messages
-        while True:
-            user_message = input()
-            self.send_message(user_message)
-            if user_message == 'q':
-                self.send_message("!DISCONNECT")
-                print(self.receive_message())
-                break
-        
-        self.communication_socket.close()
-
-    def user_query(self):
-        print("Do you want to try it again?")
-        print("y - Yes")
-        print("n - No")
-        user_input = input()
-        if user_input == 'y':
-            return
-        elif user_input == 'n':
-            exit()
 
 def main():
     print("1 - SERVER \n2 - CLIENT \n")
